a011647073bc1a3d7cc4e101e345b32b6fbf88c9
Adelshkeir <adelshkeir12@gmail.com>
2023-12-17 13:54:56 +0200

    starting

---
+++ blooming/src/pages/homepage.jsx
@@ -0,0 +1,28 @@
+import react from "react";
+import HomeCarousel from "../components/homepagecarousel";
+const HomePage = () => {
+  return (
+  <div>
+ 
+
+
+
+
+<HomeCarousel />
+    
+    Hello
+
+
+
+
+
+
+
+
+
+
+    </div>
+  )
+};
+
+export default HomePage;
